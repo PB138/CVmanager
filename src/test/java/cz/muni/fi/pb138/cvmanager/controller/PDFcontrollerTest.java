758ea158f9c13c30faa8f227e6f983f76ec119e0
Norbert Fabian <fabian.norbert@hotmail.sk>
2016-06-09 23:35:22 +0200

    Update PDFcontrollerTest with change to standalone

--- src/test/java/cz/muni/fi/pb138/cvmanager/controller/PDFcontrollerTest.java
+++ src/test/java/cz/muni/fi/pb138/cvmanager/controller/PDFcontrollerTest.java
@@ -2,9 +2,19 @@ package cz.muni.fi.pb138.cvmanager.controller;
 
 import cz.muni.fi.pb138.cvmanager.service.PDFgenerator;
 import cz.muni.fi.pb138.cvmanager.service.XmlService;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+
+import javax.xml.ws.Response;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,12 +30,25 @@ public class PDFcontrollerTest extends ControllerTestTemplate  {
     PDFgenerator pdfGenerator;
 
     @InjectMocks
-    PDFcontroller pdfController;
+    PDFcontroller pdfController = new PDFcontroller();
+
+    @Before
+    public void setup() {
+        super.setup(pdfController);
+    }
 
 
     @Test
     public void downloadPDFTest() throws Exception {
-        mockMvc.perform(get("/auth/download?l=en"))
-                .andExpect(status().isOk());
+        Mockito.when(pdfGenerator.latexToPdf(Mockito.anyString()))
+                .thenReturn(new FileInputStream(new File("cvxml/test_cv.pdf")));
+
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/auth/download?l=en")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user")))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        Assert.assertEquals("Content is not application/pdf", "application/pdf", response.getContentType());
     }
 }
