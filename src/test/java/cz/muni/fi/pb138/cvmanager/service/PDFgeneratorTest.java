ebc18744d7e5bac6a238b5f70b7660042d17e630
Norbert Fabian <fabian.norbert@hotmail.sk>
2016-06-09 23:52:33 +0200

    Update PdfGeneratorTest wit xmlToLatexTest

--- src/test/java/cz/muni/fi/pb138/cvmanager/service/PDFgeneratorTest.java
+++ src/test/java/cz/muni/fi/pb138/cvmanager/service/PDFgeneratorTest.java
@@ -1,10 +1,15 @@
 package cz.muni.fi.pb138.cvmanager.service;
 
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import cz.muni.fi.pb138.cvmanager.TestEntityFactory;
+import cz.muni.fi.pb138.cvmanager.entity.CurriculumVitae;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -20,37 +25,27 @@ import static junit.framework.TestCase.assertTrue;
 @RunWith(DataProviderRunner.class)
 public class PDFgeneratorTest {
 
+    @Mock
+    XmlService xmlService;
+
+    @InjectMocks
     PDFgenerator pdfGenerator;
 
     @Before
-    public void initialize() {
-        pdfGenerator = new PDFgenerator();
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
     }
 
-//    @Test
-//    public void xmlToLatexTest()
-//            throws ParserConfigurationException, IOException, SAXException, TransformerException
-//    {
-//        pdfGenerator.xmlToLatex("test", "sk");
-//        File file = new File("cvxml/test_cv.tex");
-//        assertTrue(file.exists());
-//        file.delete();
-//
-//        pdfGenerator.xmlToLatex("test", "en");
-//        file = new File("cvxml/test_cv.tex");
-//        assertTrue(file.exists());
-//    }
-
-
     @Test
-    public void latexToPdfTest()
-            throws IOException, InterruptedException
+    public void xmlToLatexTest()
+            throws ParserConfigurationException, IOException, SAXException, TransformerException
     {
-        File f = new File("cvxml/test_cv.tex");
-        if (f.exists()) {
-            pdfGenerator.latexToPdf("test");
-            File file = new File("cvxml/test_cv.pdf");
-            assertTrue(file.exists());
-        }
+        Mockito.when(xmlService.loadFromXml(Mockito.anyString()))
+                .thenReturn(new CurriculumVitae());
+
+        pdfGenerator.xmlToLatex("test", "sk");
+        File file = new File("cvxml/test_cv.tex");
+        assertTrue(file.exists());
+        file.delete();
     }
 }
