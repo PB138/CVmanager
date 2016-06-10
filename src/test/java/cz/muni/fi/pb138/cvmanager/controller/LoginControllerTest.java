fb5821b2ddedcfb37e225bfbce9e3eade05e6e7f
Ondrej <ozeman77@gmail.com>
2016-06-10 15:20:16 +0200

    fix: all tests except registerPost

--- src/test/java/cz/muni/fi/pb138/cvmanager/controller/LoginControllerTest.java
+++ src/test/java/cz/muni/fi/pb138/cvmanager/controller/LoginControllerTest.java
@@ -2,11 +2,16 @@ package cz.muni.fi.pb138.cvmanager.controller;
 
 import cz.muni.fi.pb138.cvmanager.entity.Account;
 import cz.muni.fi.pb138.cvmanager.service.AccountService;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,8 +26,21 @@ public class LoginControllerTest extends ControllerTestTemplate {
     AccountService accountService;
 
     @InjectMocks
-    LoginController loginController;
+    LoginController loginController = new LoginController();
 
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(loginController)
+                .apply(SecurityMockMvcConfigurers.springSecurity(filterChainProxy))
+                .setViewResolvers(viewResolver)
+                .build();
+    }
     @Test
     public void loginOkTest() throws Exception {
         mockMvc.perform(get("/login"))
@@ -53,11 +71,10 @@ public class LoginControllerTest extends ControllerTestTemplate {
 
     @Test
     public void registerPostOkTest() throws Exception {
-
         Mockito.doNothing().when(accountService).register(Mockito.any(Account.class));
 
         mockMvc.perform(post("/register").with(SecurityMockMvcRequestPostProcessors.csrf())
-                .param("username", "asdfff").param("password", "asdfff").param("retypePassword", "asdfff"))
+                .sessionAttr("account", new Account("asdf", "asdf")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login"));
     }
